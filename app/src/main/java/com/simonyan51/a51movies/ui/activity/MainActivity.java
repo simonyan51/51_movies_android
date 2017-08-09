c23413b2596b595b78767b36fad56e18f2b2b9be
simonyan51 <gnelsimonyan51@gmail.com>
2017-08-09 23:31:18 +0400

    [+] Added bottom navigation bar

--- app/src/main/java/com/simonyan51/a51movies/ui/activity/MainActivity.java
+++ app/src/main/java/com/simonyan51/a51movies/ui/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.simonyan51.a51movies.ui.activity;
 
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
 
 import com.simonyan51.a51movies.R;
 
@@ -39,6 +43,11 @@ public class MainActivity extends BaseActivity {
         return R.layout.activity_main;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_message, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
     // ===========================================================
     // Listeners, methods for/from Interfaces
     // ===========================================================
